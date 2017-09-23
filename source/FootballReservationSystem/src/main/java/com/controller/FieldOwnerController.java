ed29c484c38bcd58aacfcfed8f3dfb29118e0f15
thanhthse61493 <truonghuuthanh95@gmail.com>
2017-09-24 00:20:32 +0700

    web server create login field owner,

---
+++ source/FootballReservationSystem/src/main/java/com/controller/FieldOwnerController.java
@@ -0,0 +1,28 @@
+package com.controller;
+
+import com.entity.FieldOwnerEntity;
+import com.services.FieldOwnerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * Created by truonghuuthanh on 9/23/17.
+ */
+@RestController
+@RequestMapping(value = "/fieldowner")
+public class FieldOwnerController {
+
+    @Autowired
+    FieldOwnerService fieldOwnerService;
+
+
+    @CrossOrigin(origins = "http://localhost:3000")
+    @GetMapping(value = "/login")
+    public ResponseEntity login (@RequestParam("username") String username, @RequestParam("password") String password){
+        FieldOwnerEntity fieldOwnerEntity = fieldOwnerService.fieldOwnerLogin(username, password);
+        return new ResponseEntity(fieldOwnerEntity, HttpStatus.ACCEPTED);
+    }
+}
+
--- source/FootballReservationSystem/src/main/java/com/repository/FieldOwnerRepository.java
+++ source/FootballReservationSystem/src/main/java/com/repository/FieldOwnerRepository.java
@@ -7,4 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * Created by MinhQuy on 9/23/2017.
  */
 public interface FieldOwnerRepository extends JpaRepository<FieldOwnerEntity, Integer> {
+
+    FieldOwnerEntity findByUsernameAndPassword(String username, String password);
+
+
 }
