a98a4a72073c3d34f632c44e89504e057d10165b
PusHGoD <vip_pro@live.com>
2017-09-30 13:47:14 +0700

    Update Project

--- source/FFRS/app/src/main/java/com/capstone/ffrs/FieldDetailActivity.java
+++ source/FFRS/app/src/main/java/com/capstone/ffrs/FieldDetailActivity.java
@@ -43,45 +43,17 @@ public class FieldDetailActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.field_detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_rewards) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_logout) {
 
         }
 
--- source/FFRS/app/src/main/java/com/capstone/ffrs/FieldSuggestActivity.java
+++ source/FFRS/app/src/main/java/com/capstone/ffrs/FieldSuggestActivity.java
@@ -1,8 +1,9 @@
 package com.capstone.ffrs;
 
-import android.app.AlertDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -10,14 +11,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 
 import org.json.JSONArray;
-import org.json.JSONException;
 import org.json.JSONObject;
 
+import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
@@ -27,54 +26,20 @@ import java.util.ArrayList;
 
 public class FieldSuggestActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-//    private static final String tag = FieldSuggestActivity.class.getSimpleName();
-//    private static final String url = "https://raw.githubusercontent.com/iCodersLab/Custom-ListView-Using-Volley/master/richman.json";
-//    private List<DataSet> list = new ArrayList<>();
-//    private ListView listView;
-//    private Adapter adapter;
+    RequestQueue queue;
+
+    String url = "https://api.myjson.com/bins/w86a";
+
+    RecyclerView recyclerView;
+
+    List<NewsFeeds> feedsList = new ArrayList<NewsFeeds>();
+
+    MyRecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_field_suggest);
-//
-//        listView = (ListView) findViewById(R.id.list);
-//        adapter = new Adapter(this, list);
-//        listView.setAdapter(adapter);
-//
-//        JsonArrayRequest jsonreq = new JsonArrayRequest(url,
-//                new Response.Listener<JSONArray>() {
-//                    @Override
-//                    public void onResponse(JSONArray response) {
-//
-//                        for (int i = 0; i < response.length(); i++) {
-//                            try {
-//
-//                                JSONObject obj = response.getJSONObject(i);
-//                                DataSet dataSet = new DataSet();
-//                                dataSet.setName(obj.getString("name"));
-//                                dataSet.setWorth(obj.getString("worth"));
-//                                dataSet.setYear(obj.getInt("InYear"));
-//                                dataSet.setSource(obj.getString("source"));
-//                                list.add(dataSet);
-//                            } catch (JSONException e) {
-//                                e.printStackTrace();
-//                            }
-//
-//                        }
-//                        adapter.notifyDataSetChanged();
-//                    }
-//                }, new Response.ErrorListener() {
-//            @Override
-//            public void onErrorResponse(VolleyError error) {
-//                AlertDialog.Builder add = new AlertDialog.Builder(FieldSuggestActivity.this);
-//                add.setMessage(error.getMessage()).setCancelable(true);
-//                AlertDialog alert = add.create();
-//                alert.setTitle("Error!!!");
-//                alert.show();
-//            }
-//        });
-//        Controller.getPermission().addToRequestQueue(jsonreq);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -88,6 +53,45 @@ public class FieldSuggestActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        //Initialize RecyclerView
+        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        adapter = new MyRecyclerAdapter(this, feedsList);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        recyclerView.setAdapter(adapter);
+//Getting Instance of Volley Request Queue
+        queue = NetworkController.getInstance(this).getRequestQueue();
+//Volley's inbuilt class to make Json array request
+        JsonArrayRequest newsReq = new JsonArrayRequest(url, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+
+                        JSONObject obj = response.getJSONObject(i);
+                        NewsFeeds feeds = new NewsFeeds(obj.getString("title"), obj.getString("content"), obj.getString("image"), obj.getInt("ratebar"));
+
+// adding movie to movies array
+                        feedsList.add(feeds);
+
+                    } catch (Exception e) {
+                        System.out.println(e.getMessage());
+                    } finally {
+//Notify adapter about data changes
+                        adapter.notifyItemChanged(i);
+                    }
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                System.out.println(error.getMessage());
+            }
+        });
+//Adding JsonArrayRequest to Request Queue
+        queue.add(newsReq);
+
     }
 
     @Override
@@ -100,48 +104,21 @@ public class FieldSuggestActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.field_suggest, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_rewards) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
         } else if (id == R.id.nav_manage) {
 
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_logout) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
         }
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
