d4f8d1f2dadebc8f9a82385dfa39c7eef6545cfa
thanhthse61493 <truonghuuthanh95@gmail.com>
2017-10-02 23:09:45 +0700

    update time enable in week

--- source/web-app/src/redux/field-owner/reducers/reducers.js
+++ source/web-app/src/redux/field-owner/reducers/reducers.js
@@ -1,10 +1,11 @@
 import { combineReducers } from 'redux';
 import fieldReducer  from './field-reducer';
 import matchReducer from './match-reducer';
-
+import TimeReducer from './time-reducer';
 const reducer = combineReducers({
     field: fieldReducer,
-    match: matchReducer
+    match: matchReducer,
+    timeEnable: TimeReducer
 });
 
 export default reducer;
