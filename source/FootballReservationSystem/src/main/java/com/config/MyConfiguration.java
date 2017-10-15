8de75557ca4b9a912fdc7ed81c32be80b608f5e9
thanhthse61493 <truonghuuthanh95@gmail.com>
2017-10-15 14:29:33 +0700

    commit config

---
+++ source/FootballReservationSystem/src/main/java/com/config/MyConfiguration.java
@@ -0,0 +1,27 @@
+package com.config;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+/**
+ * Created by truonghuuthanh on 10/15/17.
+ */
+public class MyConfiguration {
+    @Bean
+    public FilterRegistrationBean corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("*");
+        config.addAllowedHeader("*");
+        config.addAllowedMethod("*");
+        source.registerCorsConfiguration("/**", config);
+        FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+        bean.setOrder(0);
+        return bean;
+    }
+}
+
