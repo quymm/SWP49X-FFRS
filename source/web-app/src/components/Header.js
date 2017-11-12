799d1b3820555e6aa395e1fccc31553bf0361c43
thanhthse61493 <truonghuuthanh95@gmail.com>
2017-11-12 16:46:25 +0700

    commit web-app

--- source/web-app/src/components/Header.js
+++ source/web-app/src/components/Header.js
@@ -280,11 +280,12 @@ class Header extends Component {
                 <i className="glyphicon glyphicon-user" />
               </Dropdown.Toggle>
               <Dropdown.Menu className="dropdown-menu-right super-colors">
+              {roleId.roleName === 'owner' ? ( <div>
                 <MenuItem onClick={this.handleUpdateProfile.bind(this)}>
                   {' '}
                   <i className="glyphicon glyphicon-user" /> Cập nhật thông tin
                 </MenuItem>
-                <MenuItem divider />
+              <MenuItem divider /> </div>) : null }
                 <MenuItem onClick={this.handleLogout.bind(this)}>
                   {' '}
                   <i className="glyphicon glyphicon-log-out" /> Đăng xuất
